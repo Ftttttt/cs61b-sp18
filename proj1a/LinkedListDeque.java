a4864e07a0127a213527f95f1c305333761cb196
Feng Tao <1078843674@qq.com>
2021-01-30 22:10:03 +0800

    LinkedListDeque completed v1

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,94 @@
+public class LinkedListDeque<T> {
+    public class Node {
+        private  Node prev;
+        private T item;
+        private Node next;
+
+        public Node(T i, Node p, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private Node sentF;
+    private int size;
+    private Node sentB;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentF = new Node(88, null, null);
+        sentB = new Node(99, null, null);
+        sentF.next = sentB;
+        sentB.prev = sentF;
+    }
+    public LinkedListDeque(T x) {
+        size = 1;
+        sentF = new Node(88, null, null);
+        sentB = new Node(99, null, null);
+        sentF.next = new Node(x, sentF, sentB);
+        sentB.prev = sentF.next;
+    }
+
+    //方法
+    public void addFirst(T item) {
+        size += 1;
+        sentF.next = new Node(item, sentF, sentF.next);
+    }
+
+    public void addLast(T item) {
+        size += 1;
+        sentB.prev = new Node(item, sentB.prev, sentB);
+    }
+
+    public boolean isEmpty() {
+        if (size = 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node ptr = sentF.next;
+        while (ptr.next != null) {
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        System.out.println(ptr.item);
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        Node temp = sentF.next;
+        sentF.next.next.prev = sentF;
+        sentF.next = sentF.next.next;
+        return temp.item;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        Node temp = sentB.prev;
+        sentB.prev.prev.next = sentB;
+        sentB.prev = sentB.prev.prev;
+        return temp.item;
+    }
+
+    public T get(int index) {
+        Node temp = sentF.next;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.item;
+    }
+
+}
